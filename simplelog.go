2ea5ae800e6547b831b05a14a1e3e8b330afe3a7
Thomas Ritter <thomas.ritter@ibm.com>
2023-04-05 16:29:05 +0200

    docu update

--- simplelog.go
+++ simplelog.go
@@ -1,6 +1,6 @@
-// Package simplelog is a logging package with the focus on ease of use.
-// It utilizes the log package from the standard library with some advanced
-// features.
+// Package simplelog is a logging package with the focus on simplicity and
+// ease of use. It utilizes the log package from the standard library with
+// some advanced features.
 // Once started, the simple logger runs as a service and listens for logging
 // requests through the functions WriteTo[Stdout|File|Multiple].
 // As the name of the WriteTo functions suggests, the simple logger writes
