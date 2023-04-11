29232cd72399d583e46afd46e6dd7a1b0e605887
Thomas Ritter <thomas.ritter@ibm.com>
2023-04-11 14:01:44 +0200

    initial

---
+++ simplelog_test.go
@@ -0,0 +1,15 @@
+package simplelog
+
+import "testing"
+
+func TestWriteToStdout(t *testing.T) {
+	// TODO: implement
+}
+
+func TestWriteToFile(t *testing.T) {
+	// TODO: implement
+}
+
+func TestWriteToMulti(t *testing.T) {
+	// TODO: implement
+}
