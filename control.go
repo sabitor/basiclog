5d7f5c7e16eaa41ef722bf25b199d5d152f0c1cb
Thomas Ritter <thomas.ritter@ibm.com>
2023-06-21 15:23:56 +0200

    added channel

--- control.go
+++ control.go
@@ -5,9 +5,10 @@ var c = new(control)
 
 // control is a structure to control and handle the communication with the log service.
 type control struct {
-	checkServiceState         chan int  // the channel for receiving a state check request from the caller
-	checkServiceStateResponse chan bool // the channel for sending a boolean response to the caller
-	serviceState              chan int  // the channel for receiving a state change request from the caller
+	checkServiceState         chan int    // the channel for receiving a state check request from the caller
+	checkServiceStateResponse chan bool   // the channel for sending a boolean response to the caller
+	serviceState              chan int    // the channel for receiving a state change request from the caller
+	resetControl              chan signal // the channel for receiving a control reset request from the caller
 }
 
 // init starts the control.
@@ -16,6 +17,7 @@ func init() {
 	c.checkServiceState = make(chan int, 1)
 	c.checkServiceStateResponse = make(chan bool, 1)
 	c.serviceState = make(chan int)
+	c.resetControl = make(chan signal)
 
 	controlRunning := make(chan bool)
 	go c.run(controlRunning)
@@ -27,6 +29,7 @@ func init() {
 // run represents the control service.
 // This utility function runs in a dedicated goroutine and is started when the init function is implicitly called.
 // It handles requests by listening on the following channels:
+//   - resetControl
 //   - serviceState
 //   - checkServiceState
 func (c *control) run(controlRunning chan bool) {
@@ -36,6 +39,9 @@ func (c *control) run(controlRunning chan bool) {
 	for {
 		select {
 		case controlRunning <- true:
+		case <-c.resetControl:
+			newState = 0
+			totalState = newState
 		case newState = <-c.serviceState:
 			if newState == stopped {
 				// unset all other states
