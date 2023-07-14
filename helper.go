baaf6f53119e0447356a4846bf8822e7cb7904e0
Thomas Ritter <thomas.ritter@ibm.com>
2023-07-14 09:12:41 +0200

    Cleanup

--- helper.go
+++ helper.go
@@ -2,7 +2,6 @@ package simplelog
 
 import (
 	"fmt"
-	// "strings"
 )
 
 // convertToString converts an parameter of type any into a parameter of type string.
@@ -11,18 +10,9 @@ func convertToString(value any) string {
 	var ok bool
 
 	if str, ok = value.(string); !ok {
-		// convert parameter into a string
+		// it's not already a string - convert it
 		str = fmt.Sprint(value)
 	}
 
 	return str
 }
-
-// parseValues parses the variadic function parameters, builds a message from them and returns it.
-// func parseValues(values []any) string {
-// 	valueList := make([]string, len(values))
-// 	for i, v := range values {
-// 		valueList[i] = convertToString(v)
-// 	}
-// 	return strings.Join(valueList, " ") // + "\n"
-// }
